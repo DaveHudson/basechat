a0d28de8be2a306bcbec207f5b808874b0ebbfb1
Dave Hudson <dave@applification.net>
2023-11-29 10:50:45 +0000

    Basic HTML architecture

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import App from "./components/app";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full bg-white">
+      <body className={`${inter.className} h-full`}>
+        <App>{children}</App>
+      </body>
     </html>
   );
 }
