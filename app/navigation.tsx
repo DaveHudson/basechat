28023d29ecefca3bb58e5d34a3b27318a8ae667a
Dave Hudson <dave@applification.net>
2023-11-30 10:51:37 +0000

    Compoentise the navigation

---
+++ app/navigation.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useEffect, useState } from "react";
+import { CommandLineIcon, ChartPieIcon, FilmIcon, HomeIcon, PhotoIcon, UsersIcon } from "@heroicons/react/24/outline";
+import { usePathname } from "next/navigation";
+
+import NavigationDesktop from "@/components/nav/nav-desktop";
+import NavigationMobile from "@/components/nav/nav-mobile";
+import Header from "@/components/nav/header";
+
+export type NavigationItem = {
+  name: string;
+  href: string;
+  icon: any;
+  current: boolean;
+};
+
+export default function App({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const navigation = [
+    { name: "Dashboard", href: "/", icon: HomeIcon, current: pathname === "/" },
+    { name: "AI Prompts", href: "/prompts", icon: CommandLineIcon, current: pathname === "/prompts" },
+    { name: "AI Chat", href: "/chat", icon: UsersIcon, current: pathname === "/chat" },
+    { name: "AI Images", href: "/images", icon: PhotoIcon, current: pathname === "/images" },
+    { name: "AI Video", href: "/videos", icon: FilmIcon, current: pathname === "/videos" },
+    { name: "AI Reports", href: "/reports", icon: ChartPieIcon, current: pathname === "/reports" },
+  ];
+
+  useEffect(() => {
+    setSidebarOpen(false);
+  }, [pathname]);
+
+  return (
+    <>
+      <div>
+        <NavigationDesktop navigation={navigation} />
+        <NavigationMobile navigation={navigation} sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className="lg:pl-72">
+          <Header setSidebarOpen={setSidebarOpen} />
+          <main className="py-10">
+            <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
