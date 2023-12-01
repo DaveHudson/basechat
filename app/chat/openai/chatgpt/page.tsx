772efd22dcb8b56d063601030fbb8d9bcf493256
Dave Hudson <dave@applification.net>
2023-12-01 17:58:56 +0000

    Basic hardcoded info slide over

--- app/chat/openai/chatgpt/page.tsx
+++ app/chat/openai/chatgpt/page.tsx
@@ -2,14 +2,29 @@
 import PromptInput from "@/components/chat/prompt-input";
 import Messages from "@/components/chat/messages";
 import { useChat } from "ai/react";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import InfoSlideOver from "@/components/chat/info-slide-over";
 
 export default function Chat() {
+  const [open, setOpen] = useState(false);
   const { input, handleInputChange, handleSubmit, messages } = useChat({
     api: "/api/chat/openai/chatgpt",
   });
 
   return (
     <>
+      <div className="flex justify-end -mt-4 lg:md:-mr-2 md:-mr-0 sm:mr-0">
+        <button
+          type="button"
+          className="relative rounded-md bg-white text-gray-500 hover:text-gray-500 focus:ring-2 focus:ring-sky-500"
+          onClick={() => setOpen(true)}
+        >
+          <span className="absolute -inset-2.5" />
+          <span className="sr-only">Close panel</span>
+          <InformationCircleIcon className="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
       <div className="flex pb-4">
         <Messages messages={messages} />
       </div>
@@ -19,6 +34,7 @@ export default function Chat() {
           <PromptInput input={input} handleInputChange={handleInputChange} />
         </form>
       </div>
+      <InfoSlideOver open={open} setOpen={setOpen} />
     </>
   );
 }
